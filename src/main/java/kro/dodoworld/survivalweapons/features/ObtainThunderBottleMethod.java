9dfa1cf5fbbe8f206c3cd03ea3c430f4388c6637
dodoman8067 <dodoman8067@gmail.com>
2023-01-11 13:13:54 +0900

    Added ThunderBottle's obtain method

--- src/main/java/kro/dodoworld/survivalweapons/features/ObtainThunderBottleMethod.java
+++ src/main/java/kro/dodoworld/survivalweapons/features/ObtainThunderBottleMethod.java
@@ -1,21 +1,17 @@
 package kro.dodoworld.survivalweapons.features;
 
 import kro.dodoworld.survivalweapons.items.ItemsInit;
-import org.bukkit.Material;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LightningStrike;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class ObtainThunderBottleMethod implements Listener {
     @EventHandler
-    public void onInteract(PlayerInteractAtEntityEvent event){
-        if(!event.getPlayer().getInventory().getItemInMainHand().equals(new ItemStack(Material.GLASS_BOTTLE))) return;
-        if(!event.getRightClicked().getType().equals(EntityType.LIGHTNING)) return;
-        ItemStack glassBottle = event.getPlayer().getInventory().getItemInMainHand();
-        glassBottle.setAmount(glassBottle.getAmount() - 1);
-        event.getPlayer().getInventory().addItem(new ItemStack(ItemsInit.ThunderBottle));
+    public void onInteract(EntityDeathEvent event){
+        if(event.getEntity().getKiller() instanceof LightningStrike){
+            event.getEntity().getLocation().getWorld().dropItemNaturally(event.getEntity().getLocation(), new ItemStack(ItemsInit.ThunderBottle));
+        }
     }
 }
