3f2cd89bbbf78db0a8cedcdb52e4eb7d949c4ad0
dodoman8067 <dodoman8067@gmail.com>
2023-01-21 11:00:52 +0900

    Fix StoneSnowBall not checking item

--- src/main/java/kro/dodoworld/survivalweapons/items/custom/StoneSnowBall.java
+++ src/main/java/kro/dodoworld/survivalweapons/items/custom/StoneSnowBall.java
@@ -1,10 +1,14 @@
 package kro.dodoworld.survivalweapons.items.custom;
 
+import kro.dodoworld.survivalweapons.items.ItemsInit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
 
 public class StoneSnowBall implements Listener {
     @EventHandler
@@ -12,10 +16,21 @@ public class StoneSnowBall implements Listener {
         if(event.getHitEntity() == null) return;
         if(!(event.getHitEntity() instanceof LivingEntity)) return;
         if(!(event.getEntity().getShooter() instanceof Player)) return;
+        if(event.getEntity() instanceof Snowball) return;
+        if(!event.getEntity().getScoreboardTags().contains("sw_stonesnowball")) return;
 
         LivingEntity entity = (LivingEntity) event.getHitEntity();
         Player player = (Player) event.getEntity().getShooter();
 
         entity.damage(1, player);
+    }
+
+    @EventHandler
+    public void onThrow(ProjectileLaunchEvent event){
+        if(!(event.getEntity().getShooter() instanceof Player)) return;
+        if(!(event.getEntity() instanceof Snowball)) return;
+        Player player = (Player) event.getEntity().getShooter();
+        if(!ItemsInit.hasLore(ChatColor.RED + "눈덩이 대미지 + 1", player)) return;
+        event.getEntity().addScoreboardTag("sw_stonesnowball");
     }
 }
