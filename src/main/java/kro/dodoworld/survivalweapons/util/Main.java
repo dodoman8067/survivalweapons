94e339a38a5f48de7a21be826eddd5c064792eee
dodoman8067 <dodoman8067@gmail.com>
2023-01-23 11:58:10 +0900

    Modify gui

--- src/main/java/kro/dodoworld/survivalweapons/util/Main.java
+++ src/main/java/kro/dodoworld/survivalweapons/util/Main.java
@@ -1,22 +1,27 @@
 package kro.dodoworld.survivalweapons.util;
 
-import javax.swing.*;
-import javax.swing.text.JTextComponent;
+import javax.swing.JLabel;
+import javax.swing.JFrame;
 import java.awt.FlowLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 /**
  * Wait, How did You found this class?
  */
 public class Main extends JFrame {
 
     private Main(){
+        JLabel label = new JLabel("그냥 플러그인 폴더에 넣으시면 됩니다!");
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+
         setSize(300, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("플러그인 적용 방법");
-
         setLayout(new FlowLayout());
-        JLabel label = new JLabel("이 파일을 plugins 폴더에 넣으세요!");
 
         this.add(label);
+        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
         setVisible(true);
     }
     public static void main(String[] args){
