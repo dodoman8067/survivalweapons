8103615484e282051f7f6873e9946599acca1361
dodoman8067 <dodoman8067@gmail.com>
2023-01-23 11:51:25 +0900

    Add gui

--- src/main/java/kro/dodoworld/survivalweapons/util/Main.java
+++ src/main/java/kro/dodoworld/survivalweapons/util/Main.java
@@ -1,10 +1,25 @@
 package kro.dodoworld.survivalweapons.util;
 
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
 /**
  * Wait, How did You found this class?
  */
-public class Main {
+public class Main extends JFrame {
+
+    private Main(){
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("플러그인 적용 방법");
+
+        setLayout(new FlowLayout());
+        JTextField field = new JTextField("그냥 서버 폴더 안에 plugins 폴더에 이 파일을 넣고 서버를 키시면 됩니다!");
+
+        this.add(field);
+        setVisible(true);
+    }
     public static void main(String[] args){
-        System.out.println("Nothing to see here...");
+        new Main();
     }
 }
